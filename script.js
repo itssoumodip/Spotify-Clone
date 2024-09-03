87e3f4e8205d63185af9f0f009a5fd113300b1f6
Soumodip Das <soumodipddas2005@gmail.com>
2024-09-03 16:01:05 +0000

    adding the songs

--- script.js
+++ script.js
@@ -126,3 +126,27 @@ document.querySelector('.showall').addEventListener('click', function() {
     cardContainer.style.overflowY = 'scroll'
     cardContainer.style.maxHeight = '60vh';
 });
+
+async function getSongs () {
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+    // console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href)
+        }    
+        }
+    return songs //return promise
+}
+
+async function main () 
+{
+    let songs = getSongs()
+    console.log(songs)
+}
+main()
